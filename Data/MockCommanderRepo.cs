c3f0da13b9fe126c14e8e19427843463ce196a10
W.C.HEW <hew1997@yahoo.com>
2020-09-14 23:43:13 +0800

    Added mock repo

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>{
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="linux"},
+                new Command{Id=1, HowTo="Cut a bread", Line="Cut bread", Platform="linux"},
+                new Command{Id=2, HowTo="Make cup of tea", Line="Make tea", Platform="linux"},
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=id, HowTo="Boil an egg", Line="Boil water", Platform="linux"};
+        }
+    }
+}
