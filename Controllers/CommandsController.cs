4cfcd449293d8af66cc354d283d27a641bf95a17
W.C.HEW <hew1997@yahoo.com>
2020-10-19 18:02:52 +0800

    Added create resource

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -30,7 +30,7 @@ namespace Commander.Controllers
         }
 
         //api/commands/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetCommandById")]
         public ActionResult <CommandReadDto> GetCommandById(int id){
             var commandItem = _repository.GetCommandById(id);
             if(commandItem != null)
@@ -38,6 +38,17 @@ namespace Commander.Controllers
             else
                 return NotFound();
         
+        }
+
+        //POST api/commands/
+        [HttpPost()]
+        public ActionResult <CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto){
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.saveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+            return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
         }
     }
 }
--- Data/ICommanderRepo.cs
+++ Data/ICommanderRepo.cs
@@ -5,8 +5,10 @@ namespace Commander.Data
 {
     public interface ICommanderRepo
     {
+        bool saveChanges();
         IEnumerable<Command> GetAllCommands();
         Command GetCommandById(int id);
+        void CreateCommand(Command cmd);
         
     }
 }
