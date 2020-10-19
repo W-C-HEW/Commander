bd806a2611675484c737504982215a5acb9e8c56
W.C.HEW <hew1997@yahoo.com>
2020-10-19 16:26:19 +0800

    Implemented DTO

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,25 +13,30 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
-        public CommandsController(ICommanderRepo repository)
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             //repository is passed here from startup service.AddScoped
             _repository = repository;
+            _mapper = mapper;
         }
         
         //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands(){
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands(){
             var commandItems = _repository.GetAllCommands();
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
         //api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id){
+        public ActionResult <CommandReadDto> GetCommandById(int id){
             var commandItem = _repository.GetCommandById(id);
-            return Ok(commandItem);
+            if(commandItem != null)
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            else
+                return NotFound();
         
         }
     }
